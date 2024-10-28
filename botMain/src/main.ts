f1ee079ba33208e9ff9707e8518ea403fb44f5f0
denisbondarenko <last.head@yandex.ru>
2024-10-28 18:20:06 +0300

    add swagger api

--- botMain/src/main.ts
+++ botMain/src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function start() {
   const PORT = process.env.PORT || 5000
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('POSHAGAM API')
+    .setDescription('документация для магазина кроссовок')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/swagger', app, document);
+
   await app.listen(PORT, () => {console.log(`Server started on port = ${PORT}`)});
 }
 start();
